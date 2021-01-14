5cd1c56cab024831b709ba94b14b51d8dba71c05
Karolina Rogowska <karolina.rogowska@intel.com>
2021-01-14 11:51:09 +0100

    add API for LVM (Logical Volume Manager)
    Signed-off-by: Karolina Rogowska <karolina.rogowska@intel.com>

--- test_tools/disk_utils.py
+++ test_tools/disk_utils.py
@@ -326,7 +326,13 @@ def _is_by_id_path(path: str):
 
 def _is_dev_path_whitelisted(path: str):
     """check if given path is whitelisted"""
-    return re.search(r"cas\d+-\d+", path) is not None
+    whitelisted_paths = [r"cas\d+-\d+", r"/dev/dm-\d+"]
+
+    for whitelisted_path in whitelisted_paths:
+        if re.search(whitelisted_path, path) is not None:
+            return True
+
+    return False
 
 
 def _is_dev_path_blacklisted(path: str):
