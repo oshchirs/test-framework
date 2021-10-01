88438ca497c6c2c14374d36eae1bbe488a83a142
Pawel Lewalski <pawel.lewalski@intel.com>
2021-10-01 14:43:42 -0700

    Command_param refactor.
    Refactoring 'command_param_dict' to 'command_param'.
    Refactoring 'command_env_var_dict' to 'command_env_var'.

--- test_utils/linux_command.py
+++ test_utils/linux_command.py
@@ -9,14 +9,14 @@ from collections import defaultdict
 class LinuxCommand:
     def __init__(self, command_executor, command_name):
         self.command_executor = command_executor
-        self.command_param_dict = defaultdict(list)
+        self.command_param = defaultdict(list)
         self.command_flags = []
         self.command_name = command_name
         self.param_name_prefix = ''
         self.param_separator = ' '
         self.param_value_prefix = '='
         self.param_value_list_separator = ','
-        self.command_env_var_dict = defaultdict(list)
+        self.command_env_var = defaultdict(list)
         self.env_var_separator = ' '
         self.env_var_value_prefix = '='
 
@@ -40,38 +40,38 @@ class LinuxCommand:
         self.remove_param(key)
 
         for val in values:
-            self.command_param_dict[key].append(str(val))
+            self.command_param[key].append(str(val))
         return self
 
     def remove_param(self, key):
-        if key in self.command_param_dict:
-            del self.command_param_dict[key]
+        if key in self.command_param:
+            del self.command_param[key]
         return self
 
     def set_env_var(self, key, *values):
         self.remove_env_var(key)
 
         for val in values:
-            self.command_env_var_dict[key].append(str(val))
+            self.command_env_var[key].append(str(val))
         return self
 
     def remove_env_var(self, key):
-        if key in self.command_env_var_dict:
-            del self.command_env_var_dict[key]
+        if key in self.command_env_var:
+            del self.command_env_var[key]
         return self
 
     def get_parameter_value(self, param_name):
-        if param_name in self.command_param_dict.keys():
-            return self.command_param_dict[param_name]
+        if param_name in self.command_param.keys():
+            return self.command_param[param_name]
         return None
 
     def __str__(self):
         command = ''
-        for key, value in self.command_env_var_dict.items():
+        for key, value in self.command_env_var.items():
             command += f'{key}{self.env_var_value_prefix}{",".join(value)}' \
                        f'{self.env_var_separator}'
         command += self.command_name
-        for key, value in self.command_param_dict.items():
+        for key, value in self.command_param.items():
             command += f'{self.param_separator}{self.param_name_prefix}' \
                 f'{key}{self.param_value_prefix}{",".join(value)}'
         for flag in self.command_flags:
